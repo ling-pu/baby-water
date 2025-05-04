cc21b3aa91897e889d8bb4d9cc44f2dba2bbf0a4
ling-pu <impu317@gmail.com>
2025-05-04 17:13:39 +0800

    0504-2

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,30 @@ import './App.css'
 import Navbar from './component/Navbar'
 import { Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Japan from './pages/Japan'
+import World from './pages/World'
+import Timesale from './pages/Timesale'
+import User from './pages/User'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
       <div className="wrap">
         {/* 載入導覽列 */}
         <Navbar>
           {/* 呼叫頁面 */}
           <Routes>
             <Routes path="/" element={<Home/>} />
+            <Routes path="/japan" element={<Japan/>} />
+            <Routes path="/world" element={<World/>} />
+            <Routes path="/timesale" element={<Timesale/>} />
+            <Routes path="/user" element={<User/>} />
           </Routes>
-
         </Navbar>
 
       </div>
 
-
-    </>
   )
 }
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar(){
     return(
+        
         <nav className="Navbar">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+            <Link to="/">首頁</Link>
+            <Link to="/japan">日本代購</Link>
+            <Link to="/world">世界選品</Link>
+            <Link to="/timesale">每週超優惠</Link>
         </nav>
     )
 }
