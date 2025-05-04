7b05ec8f8df1f1900d457960005ed2d74edf124b
ling-pu <impu317@gmail.com>
2025-05-04 23:38:12 +0800

    0504-4

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,18 +4,16 @@ import logo from "/logo/logo.svg";
 export default function Header() {
     return (
         <>
-            <header className="siteHeader-PC">
-                <Link to="/">
-                    <h1 style={{ margin: 0 }}>
-                        <img src={logo} alt="水寶日本代購 Baby Water Online Store" style={{ height: '40px' }} />
-                    </h1>
-                </Link>
-                <h1><a href="/"><img src="" alt="" /></a></h1>
-                <nav className="Navbar">
-                    <Link to="/">首頁</Link>
-                    <Link to="/japan">日本代購</Link>
-                    <Link to="/world">世界選品</Link>
-                    <Link to="/timesale">每週超優惠</Link>
+            <header className="siteHeader isPC">
+                <h1 className="logo">
+                    <Link to="/"><img src={logo} alt="水寶日本代購 Baby Water Online Store" /></Link>
+                </h1>
+                <nav className="navbar">
+                    <ul>
+                        <li><Link to="/japan">日本代購</Link></li>
+                        <li><Link to="/world">世界選品</Link></li>
+                        <li><Link to="/timesale">每週超優惠</Link></li>
+                    </ul>
                 </nav>
 
             </header>
