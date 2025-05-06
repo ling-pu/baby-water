1bbddd9e4d83bd99a2f9df7d31da777331ffdda6
ling-pu <impu317@gmail.com>
2025-05-06 11:52:04 +0800

    0506-1

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,29 @@ import Japan from './pages/Japan'
 import World from './pages/World'
 import Timesale from './pages/Timesale'
 import User from './pages/User'
+import Announcement from './component/Announcement'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div className="wrap">
-        {/* 載入導覽列 */}
-        <Header>
-          {/* 呼叫頁面 */}
-          <Routes>
-            <Routes path="/" element={<Home/>} />
-            <Routes path="/japan" element={<Japan/>} />
-            <Routes path="/world" element={<World/>} />
-            <Routes path="/timesale" element={<Timesale/>} />
-            <Routes path="/user" element={<User/>} />
-          </Routes>
-        </Header>
+    <div className="wrap">
+      {/* 文字跑馬燈 */}
+      <Announcement/>
+      
+      {/* 載入導覽列 */}
+      <Header>
+        {/* 呼叫頁面 */}
+        <Routes>
+          <Routes path="/" element={<Home />} />
+          <Routes path="/japan" element={<Japan />} />
+          <Routes path="/world" element={<World />} />
+          <Routes path="/timesale" element={<Timesale />} />
+          <Routes path="/user" element={<User />} />
+        </Routes>
+      </Header>
 
-      </div>
+    </div>
 
   )
 }
