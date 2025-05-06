adaf1e791ced28054e1e8a70881b0fb318e36598
ling-pu <impu317@gmail.com>
2025-05-06 11:52:52 +0800

    0506-1

---
+++ src/component/Announcement.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Announcement() {
+    return (
+        <div className="announcement">
+            <div className="announcement__text">
+                <p className="announcement__title">
+                    <span>Baby Water官網會員招募中 ♡ 註冊即享$100購物金！</span>
+                </p>
+            </div>
+        </div>
+    )
+}
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -8,9 +8,16 @@ export default function Header() {
                 <h1 className="logo">
                     <Link to="/"><img src={logo} alt="水寶日本代購 Baby Water Online Store" /></Link>
                 </h1>
-                <nav className="navbar">
+                <nav className="action-menu">
                     <ul>
                         <li><Link to="/japan">日本代購</Link></li>
+                        <li><Link to="/world">世界選品</Link></li>
+                        <li><Link to="/timesale">每週超優惠</Link></li>
+                    </ul>
+                </nav>
+                <nav className="main-menu">
+                    <ul>
+                        <li><Link to="/user">日本代購</Link></li>
                         <li><Link to="/world">世界選品</Link></li>
                         <li><Link to="/timesale">每週超優惠</Link></li>
                     </ul>
