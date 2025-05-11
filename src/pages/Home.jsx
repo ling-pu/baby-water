ddf7e432072b1bfe35926dcb4d2ea9edf9ecebe6
ling-pu <impu317@gmail.com>
2025-05-11 20:17:32 +0800

    0511-2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
+import banner from "../assets/banner.gif"
 export default function Home() {
     return (
       <>
-     <div className="banner-container">
-      </div>
-
       <body className="home">
+        <section className="banner-container">
+        <img src={banner} alt="" />
+        </section>
+        <section className="display-container">
         <h1>Baby Water Select</h1>
+
+        </section>
+        <section className="display-container">
+          
+        </section>
+   
+
+        
         <div className="home-select">
 
         </div>
