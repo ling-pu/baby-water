cb470b9f28714f231aef248cafd50ff394321099
ling-pu <impu317@gmail.com>
2025-05-11 16:54:10 +0800

    0511-1

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -30,36 +30,36 @@ export default function Header() {
                         <Link to="/"><img src={logo} alt="水寶日本代購 Baby Water Online Store" /></Link>
                     </h1>
                 </div>
-                {/* 導覽列區 2列 */}
-                <div className="nav-container">
-                    <nav className="icon-nav">
-                        <ul className="icon">
-                            <li>
-                                <button className="icon-btn icon-search"
-                                    onClick={() => handleClick('search')}
-                                    title="搜尋"></button>
-                            </li>
-                            <li>
-                                <button
-                                    className="icon-btn icon-user"
-                                    onClick={() => handleClick('user')}
-                                    title="會員中心"></button>
-                            </li>
-                            <li>
-                                <button className="icon-btn icon-cart"
-                                    onClick={() => handleClick('cart')}
-                                    title="購物車"></button>
-                            </li>
-                        </ul>
-                    </nav>
-                    <nav className="main-nav">
-                        <ul>
-                            <li><Link to="/japan">日本代購</Link></li>
-                            <li><Link to="/world">世界選品</Link></li>
-                            <li><Link to="/timesale">每週超優惠</Link></li>
-                        </ul>
-                    </nav>
-                </div>
+                {/* 導覽列區 */}
+                <nav className="main-nav">
+                    <ul>
+                        <li><Link to="/japan">日本代購</Link></li>
+                        <li><Link to="/world">世界選品</Link></li>
+                        <li><Link to="/timesale">每週超優惠</Link></li>
+                    </ul>
+                </nav>
+                <nav className="icon-nav">
+                    <ul className="icon">
+                        <li>
+                            <button className="icon-btn icon-search"
+                                onClick={() => handleClick('search')}
+                                title="搜尋"></button>
+                        </li>
+                        <li>
+                            <button
+                                className="icon-btn icon-user"
+                                onClick={() => handleClick('user')}
+                                title="會員中心"></button>
+                        </li>
+                        <li>
+                            <button className="icon-btn icon-cart"
+                                onClick={() => handleClick('cart')}
+                                title="購物車"></button>
+                        </li>
+                    </ul>
+                </nav>
+
+
             </header>
         </>
 
