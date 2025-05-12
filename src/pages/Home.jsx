c3987ebe724e39c8e78c91cbb02b29700c245f8c
ling-pu <impu317@gmail.com>
2025-05-12 22:59:51 +0800

    0512-1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import banner from "../assets/banner.gif"
+import bg from "../assets/bg.svg"
+
 export default function Home() {
     return (
       <>
@@ -6,13 +8,38 @@ export default function Home() {
         <section className="banner-container">
         <img src={banner} alt="" />
         </section>
-        <section className="display-container">
-        <h1>Baby Water Select</h1>
+        
+        <section className="pick">
+          <div>
+          <h1 className="title">Baby Water’s  Pick</h1>
+          <h1 className="timesale">Time Sale ~ 05/30</h1>
+          </div>
+          <div className="content">
+            {/* 1列4欄 */}
+            <div className="cardlist">
+            </div>
+            {/* btn */}
+            <div className="btn-container">
+              <a href="#" className="btn-seemore">see more</a>
+            </div>
+          </div>
+         </section>
 
-        </section>
-        <section className="display-container">
-          
-        </section>
+         <section className="world">
+          <div>
+          <h1 className="title">World Special</h1>
+          <h1 className="timesale">Time Sale ~ 06/10</h1>
+          </div>
+          <div className="content">
+            {/* 1列4欄 */}
+            <div className="cardlist">
+            </div>
+            {/* btn */}
+            <div className="btn-container">
+              <a href="#" className="btn-seemore">see more</a>
+            </div>
+          </div>
+         </section>
    
 
         
