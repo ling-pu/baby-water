71cef03267d16bd35e6b8c38ce922ae42eedbf5c
ling-pu <impu317@gmail.com>
2025-05-15 15:00:50 +0800

    0514-1

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,18 +1,33 @@
 export default function Footer() {
-    return (
-      <>
+  return (
+    <>
       <div id="footer">
-        <div className="footer-container">
-            <div className="media">
-                <h2>Follow us</h2>
-                <div className="footer-icon-container">
-                    <img src="/icons/IG.svg" alt="instagram" />
-                    <img src="/icons/line.svg" alt="line" />
-                </div>
+        <div className="footer-row1">
+          <div className="follow-us">
+            <h2>Follow us</h2>
+            <div className="footer-icon-container">
+              <img src="/icons/IG.svg" alt="instagram" />
+              <img src="/icons/line.svg" alt="line" />
             </div>
+          </div>
+          <div className="contact-us">
+            <h2>Contact us</h2>
+            <div>
+              <p>官方line : @babywater</p>
+            </div>
+          </div>
+          
         </div>
+        <div className="copyright">
+            copyright © 2025 babywater.All rights reserved.
+          </div>
+          <div className="terms">
+            <span>服務條款</span> | 
+            <span>隱私政策</span> | 
+            <span>退款政策</span>
+          </div>
       </div>
-      </>
+    </>
 
-    )
-  }
+  )
+}
