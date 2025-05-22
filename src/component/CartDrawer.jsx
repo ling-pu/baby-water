b84785a8af3686d9a1e2f4b224ba360c82502157
ling-pu <impu317@gmail.com>
2025-05-22 23:38:30 +0800

    0522-2

--- src/component/CartDrawer.jsx
+++ src/component/CartDrawer.jsx
@@ -41,10 +41,28 @@ export default function CartDrawer({ isOpen, onClose }) {
                     {/* 阻止點擊內部時觸發關閉 */}
                     <div className="cart-drawer-panel" onClick={(e) => e.stopPropagation()} >
                         <button className="close-btn" onClick={onClose}>×</button>
+                        {/* 標題 */}
                         <h2>購物車商品</h2>
-                        <div class="showProgress-stoke">
-                            <div class="showProgress-solid"></div>
+                        {/* 進度條 */}
+                        <div className="showProgress-stoke">
+                            <div className="showProgress-solid"></div>
                         </div>
+                        {/* 內容區 */}
+                        <div className="content">
+                            <div className='total'>
+                                <p>全部商品（<span>2</span>件）</p>
+                                <p>NT$ <span>2,040</span></p>
+                            </div>
+                            {/* 卡片 */}
+                            <div className='buyItemCard'>
+                                <div className="col">
+                                    <img src="/images/tops/1/1.jpg" alt="" className="pic-s" />
+                                </div>
+                                <p>NT$ <span>2,040</span></p>
+
+                            </div>
+                        </div>
+
                         <p>這裡顯示購物車內容</p>
                     </div>
                 </div>
