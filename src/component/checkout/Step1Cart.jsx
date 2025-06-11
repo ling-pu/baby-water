2f39b878268ea70dccb919649772703c4e155f40
ling-pu <impu317@gmail.com>
2025-06-11 18:53:56 +0800

    0611-3

--- src/component/checkout/Step1Cart.jsx
+++ src/component/checkout/Step1Cart.jsx
@@ -27,65 +27,75 @@ export default function Step1Cart({ onNext }) {
   return (
     <>
       <main className="checkout">
+
         <ProgressBar />
-        {/* 購物車標題 */}
-        <section className="cart-title">
-          {/* 左 */}
-          <div className="title-l">
-            <p>全部商品 ( <span> 4 </span> 件 )</p>
-          </div>
-          {/* 右 */}
-          <div className="title-r">
-            <p>總金額</p>
-            <p className="price">NT$ <span>4,080</span> </p>
-          </div>
-
-        </section>
-        {/* 購物車內容 */}
-        <section className="cart-list">
-          {/* 卡片 */}
-          <div className="cart-buyItemCard">
-            <img src="" alt="" className="pic-s" />
-
-          </div>
-          {cartItems.map((item, index) => (
-            <div key={index} className="cart-buyItem">
-              <img src={addBase(item.image)} alt={item.title} className="pic-s" />
-              <div className="info">
-                {/* 產品名稱 */}
-                <p className="title">{item.title}</p>
-                {/* 其他文字 */}
-                <p className="else">款式：{item.style}</p>
-                <p className="else">尺寸：{item.size}</p>
-                {/* <p>數量：{item.quantity}</p> */}
-                <div className="item-number">
-                  <button >−</button>
-                  <div className="number-area">
-                    <span>{item.quantity}</span>
-                  </div>
-                  <button >＋</button>
-                </div>
-                {/* <p>單價：NT$ {item.price.toLocaleString()}</p> */}
-
-              </div>
-              {/* 金額 */}
-              <p>NT$ <span>{(item.price * item.quantity).toLocaleString()}</span></p>
-              {/* 刪除按鈕 */}
-              <button onClick={() => removeFromCart(index)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
-                  <g clip-path="url(#clip0_1035_1583)">
-                    <path d="M15 8.21476L10 13.2148M10 8.21476L15 13.2148M17.5 4.0481H6.66671L0.833374 10.7148L6.66671 17.3814H17.5C17.9421 17.3814 18.366 17.2058 18.6786 16.8933C18.9911 16.5807 19.1667 16.1568 19.1667 15.7148V5.71476C19.1667 5.27273 18.9911 4.84881 18.6786 4.53625C18.366 4.22369 17.9421 4.0481 17.5 4.0481Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_1035_1583">
-                      <rect width="20" height="20" fill="white" transform="translate(0 0.714844)" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </button>
+        <div className="scroll-area">
+          {/* 購物車標題 */}
+          <section className="cart-title">
+            {/* 左 */}
+            <div className="title-l">
+              <p>全部商品 ( <span> {totalQuantity} </span> 件 )</p>
             </div>
-          ))}
-        </section>
+            {/* 右 */}
+            <div className="title-r">
+              <p>總金額</p>
+              <p className="price">NT$ <span>{totalPrice.toLocaleString()}</span> </p>
+            </div>
+
+          </section>
+          {/* 購物車內容 */}
+          <section className="cart-list">
+            {/* 卡片 */}
+            <div className="cart-buyItemCard">
+              <img src="" alt="" className="pic-s" />
+
+            </div>
+            {cartItems.map((item, index) => (
+              <div key={index} className="cart-buyItem">
+                <img src={addBase(item.image)} alt={item.title} className="pic-s" />
+                <div className="info">
+                  {/* 產品名稱 */}
+                  <p className="title">{item.title}</p>
+                  {/* 其他文字 */}
+                  <p className="else">款式：{item.style}</p>
+                  <p className="else">尺寸：{item.size}</p>
+                  {/* <p>數量：{item.quantity}</p> */}
+                  <div className="item-number">
+                    <button >−</button>
+                    <div className="number-area">
+                      <span>{item.quantity}</span>
+                    </div>
+                    <button >＋</button>
+                  </div>
+                  {/* <p>單價：NT$ {item.price.toLocaleString()}</p> */}
+
+                </div>
+                {/* 金額 */}
+                <p>NT$ <span>{(item.price * item.quantity).toLocaleString()}</span></p>
+                {/* 刪除按鈕 */}
+                <button onClick={() => removeFromCart(index)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
+                    <g clip-path="url(#clip0_1035_1583)">
+                      <path d="M15 8.21476L10 13.2148M10 8.21476L15 13.2148M17.5 4.0481H6.66671L0.833374 10.7148L6.66671 17.3814H17.5C17.9421 17.3814 18.366 17.2058 18.6786 16.8933C18.9911 16.5807 19.1667 16.1568 19.1667 15.7148V5.71476C19.1667 5.27273 18.9911 4.84881 18.6786 4.53625C18.366 4.22369 17.9421 4.0481 17.5 4.0481Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_1035_1583">
+                        <rect width="20" height="20" fill="white" transform="translate(0 0.714844)" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </button>
+              </div>
+            ))}
+          </section>
+
+        </div>
+
+        <div className="actions">
+          <button className="next-btn" onClick={onNext}>下一步：填寫資料</button>
+        </div>
+
+
 
       </main>
 
@@ -117,7 +127,5 @@ export default function Step1Cart({ onNext }) {
 //   <p>共 {totalQuantity} 件商品，總金額：NT$ {totalPrice.toLocaleString()}</p>
 // </div>
 
-// <div className="actions">
-//   <button className="next-btn" onClick={onNext}>下一步：填寫資料</button>
-// </div>
+
 // </div>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -11,17 +11,21 @@ import ProductPage from "./ProductPage"
 export default function Pages() {
     return (
         <>
-            {/* 呼叫頁面 */}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/japan" element={<Japan />} />
-                <Route path="/world" element={<World />} />
-                <Route path="/timesale" element={<Timesale />} />
-                <Route path="/product/:id" element={<ProductPage />} />
-                <Route path="/user" element={<User />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <div className="page-wrapper">
+                {/* 呼叫頁面 */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/japan" element={<Japan />} />
+                    <Route path="/world" element={<World />} />
+                    <Route path="/timesale" element={<Timesale />} />
+                    <Route path="/product/:id" element={<ProductPage />} />
+                    <Route path="/user" element={<User />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+
+            </div>
+
 
         </>
 
